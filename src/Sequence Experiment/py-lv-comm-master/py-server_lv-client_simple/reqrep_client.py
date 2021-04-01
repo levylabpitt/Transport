cf934171fcaf11ff5b618d6550247e95d0ec89b8
Patrick Irvin <p.irvin@levylab.org>
2021-03-31 21:34:12 -0400

    updated python scripts

--- src/Sequence Experiment/py-lv-comm-master/py-server_lv-client_simple/reqrep_client.py
+++ src/Sequence Experiment/py-lv-comm-master/py-server_lv-client_simple/reqrep_client.py
@@ -1,11 +1,53 @@
 import zmq
 import sys
 
+# play with zmq stuff
+
+version = zmq.pyzmq_version()
+print(f'pyzmq version = {version}')
+
+# Provide two ports of two different servers to connect to simultaneously.
+
 port = "5556"
 if len(sys.argv) > 1:
-    port =  sys.argv[1]
-    int(port)
+    port1 =  sys.argv[1]
+    int(port1)
 
 if len(sys.argv) > 2:
-    port1 =  sys.argv[2]
-    int(port1)
+    port2 =  sys.argv[2]
+    int(port2)
+
+# Client is created with a socket type “zmq.REQ”.
+# You should notice that the same socket can connect to two different servers.
+# connect socket
+context = zmq.Context()
+print(f'Connecting to server...')
+socket = context.socket(zmq.REQ)
+
+socket.connect(f'tcp://localhost:{port1}')
+print(f'Connected to Server tcp://localhost:{port1}') 
+
+if len(sys.argv) > 2:
+	print(f'Connected to tcp://localhost: {port2}')
+	socket.connect(f'tcp://localhost:{port2}')
+
+
+# You have to send a request and then wait for reply.
+# Do 10 requests, waiting each time for a response
+for request in range(10):
+	print(f'\nSending request {request}')
+	socket.send(b"Hello")
+    #  Get the reply.
+	message = socket.recv()
+	print(f'Received reply {request} [ {message} ]')
+	print(f'Message type: {type(message)}')
+	print(f'Decoded message: {message.decode()}')
+	print(f'Decoded message type: {type(message.decode())}')
+
+# disconnect socket
+socket.disconnect(f'tcp://localhost:{port1}')
+print(f'Disconnected from Server tcp://localhost:{port1}')
+
+if len(sys.argv) > 2:
+	print(f'Disconnected from tcp://localhost:{port2}')
+	socket.connect(f'tcp://localhost:{port2}')
--- src/Sequence Experiment/py-lv-comm-master/py-server_lv-client_simple/reqrep_server.py
+++ src/Sequence Experiment/py-lv-comm-master/py-server_lv-client_simple/reqrep_server.py
@@ -1,21 +1,42 @@
 import zmq
 import time
+from datetime import datetime
 import sys
+
+print(f'Starting server...')
 
 # Provide port as command line argument to run server at two different ports.
 port = "5556"
 if len(sys.argv) > 1:
     port =  sys.argv[1]
     int(port)
+	
+print(port)
 
-
+# Server is created with a socket type “zmq.REP” and is bound to well known port.
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:%s" % port)
+socket.bind(f'tcp://*:{port}')
 
+'''
 while True:
     #  Wait for next request from client
     message = socket.recv()
-    print "Received request: ", message
-    time.sleep (1)  
-    socket.send("World from %s" % port)
+    print("Received request: ", message)
+    time.sleep(1)  
+    socket.send_string("World from %s" % port)
+'''
+	
+while True:
+	#  Wait for next request from client
+	message = socket.recv()
+	print(f'Received request: {message}')
+	
+	#  Do some 'work'
+	time.sleep(1)
+	
+	#  Send reply back to client
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	send_message = f'Hello the time is {current_time}'
+	socket.send(bytearray(send_message, 'utf-8'))
